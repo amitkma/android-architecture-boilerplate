ce1420e85d7cf38a3f39bb4f74de254388be8425
Amit Kumar <fbpass.res14@gmail.com>
2017-09-28 21:22:43 +0530

    Add formatting according to code style

--- presentation/src/main/java/com/github/amitkma/boilerplate/presentation/factory/ViewModelFactory.java
+++ presentation/src/main/java/com/github/amitkma/boilerplate/presentation/factory/ViewModelFactory.java
@@ -26,22 +26,23 @@ import javax.inject.Provider;
 import javax.inject.Singleton;
 
 @Singleton
-public class ViewModelFactory implements ViewModelProvider.Factory{
+public class ViewModelFactory implements ViewModelProvider.Factory {
 
-    private final Map<Class<? extends ViewModel>, Provider<ViewModel>> creators;
+    private final Map<Class<? extends ViewModel>, Provider<ViewModel>> mCreators;
 
     @Inject
     public ViewModelFactory(Map<Class<? extends ViewModel>, Provider<ViewModel>> creators) {
-        this.creators = creators;
+        this.mCreators = creators;
     }
 
 
     @SuppressWarnings("unchecked")
     @Override
     public <T extends ViewModel> T create(Class<T> modelClass) {
-        Provider<? extends ViewModel> creator = creators.get(modelClass);
+        Provider<? extends ViewModel> creator = mCreators.get(modelClass);
         if (creator == null) {
-            for (Map.Entry<Class<? extends ViewModel>, Provider<ViewModel>> entry : creators.entrySet()) {
+            for (Map.Entry<Class<? extends ViewModel>, Provider<ViewModel>> entry : mCreators
+                    .entrySet()) {
                 if (modelClass.isAssignableFrom(entry.getKey())) {
                     creator = entry.getValue();
                     break;
